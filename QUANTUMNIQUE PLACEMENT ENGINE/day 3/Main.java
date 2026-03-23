9776b233781536da82a079271aacc2f447e17145
Rithish <rithishk108@gmail.com>
2026-03-23 09:52:01 +0530

    Add files via upload

---
+++ QUANTUMNIQUE PLACEMENT ENGINE/day 3/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int left = 0;
+        int right = str.length() - 1;
+        boolean isPalindrome = true;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+        System.out.println(isPalindrome);
+    }
+}
